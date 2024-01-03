01891a35204d590c294f723b83d0f578fe485328
ryanb <rb593@byu.edu>
2024-01-03 08:13:00 -0600

    Set up 12-8 Part 1

---
+++ 12-8-2023/solution.py
@@ -0,0 +1,16 @@
+
+class Node:
+    left = ""
+    right = ""
+
+def main(fileName):
+    file = open(fileName, "r")
+    numberOfSteps = 0
+
+    
+
+    print("# of Steps: {0}".format(numberOfSteps))
+
+
+debug=0
+main(".\\12-8-2023\\exampleInput1.txt")
