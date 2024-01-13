adb1a49387e17a59f5e90cd61619f2e50f47fcb8
ryanb <rb593@byu.edu>
2024-01-13 13:15:09 -0600

    Solve 12-8 Part 1

--- 12-8-2023/solution.py
+++ 12-8-2023/solution.py
@@ -1,16 +1,54 @@
+import re
+
 
 class Node:
+    name = ""
     left = ""
     right = ""
+    def __init__(self, name, left, right):
+        self.name = name
+        self.left = left
+        self.right = right
+
+def getNodeWithName(nodeSet, nodeName):
+    for node in nodeSet:
+        if node.name == nodeName:
+            return node
 
 def main(fileName):
     file = open(fileName, "r")
-    numberOfSteps = 0
+    stepCount = 0
+    instructions = ""
+    nodeSet = set()
 
-    
+    for line in file:
+        if instructions == "": 
+            instructions = " ".join(re.findall("[a-zA-Z]+", line))
+        else:
+            labels = " ".join(re.findall("[a-zA-Z]+", line))
+            if labels == "": continue
+            else:
+                label1, label2, label3 = labels.split()
+                if debug: print("Words = \"{0}\" \"{1}\" \"{2}\"".format(label1, label2, label3))
 
-    print("# of Steps: {0}".format(numberOfSteps))
+                node = Node(label1, label2, label3)
+                nodeSet.add(node)
+                
+                # Set up first node
+                if label1 == "AAA": currentNode = node
+
+    while currentNode.name != "ZZZ":
+        currentInstruction = instructions[(stepCount) % len(instructions)]
+        stepCount += 1
+
+        if currentInstruction == "L":
+            nextLabel = currentNode.left
+        elif currentInstruction == "R":
+            nextLabel = currentNode.right
+        currentNode = getNodeWithName(nodeSet, nextLabel)
+
+    print("# of Steps: {0}".format(stepCount))
 
 
 debug=0
-main(".\\12-8-2023\\exampleInput1.txt")
+main(".\\12-8-2023\\input.txt")
