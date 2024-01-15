5d2b1bf947daa741107d32d4dc836d0fd09e474e
ryanb <rb593@byu.edu>
2024-01-15 10:44:29 -0600

    Solve 12-9 Part 1

---
+++ 12-9-2023/solution.py
@@ -0,0 +1,54 @@
+def GetNextReadingValue(inputArray):
+    if IsArrayZeros(inputArray):
+        return 0
+    
+    # Create difAry
+    difAry = []
+    for index in range(len(inputArray) - 1):
+        difference = inputArray[index + 1] - inputArray[index]
+        difAry.append(difference)
+    nextDifference = GetNextReadingValue(difAry)
+
+    # Get next value, add to array
+    nextValue = inputArray[len(inputArray) - 1] + nextDifference
+    return nextValue
+
+def IsArrayZeros(inputArray):
+    for value in inputArray:
+        if value != 0: return False
+    return True
+
+def main(fileName):
+    # Setup
+    file = open(fileName, "r")
+    summation = 0
+    nextReadings = []
+
+    # Find next reading for each line
+    for line in file:
+        # Read in line data
+        readingsDirty = line.split()
+        readings = []
+
+        # Convert data to integers
+        for reading in readingsDirty:
+            readings.append(int(reading))
+        
+        nextReadings.append(GetNextReadingValue(readings))
+
+    # Add next reading values together
+    for nextReading in nextReadings:
+        summation += nextReading
+
+    # Show solution
+    print("Sum of values: {0}".format(summation))
+
+debug=1
+main(".\\12-9-2023\\input.txt")
+
+# Recursive Algorithm GetNextReading(inputArray):
+    # Base case: If inputArray is all 0's, return 0
+    # Recursive Case: 
+        # Create array of differences "difAry"
+        # Call GetNextReading(difAry)
+        # Return next value using result of ^ and last value of inputArray
